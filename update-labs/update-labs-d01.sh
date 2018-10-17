2eea0540a32689a28f04a56bf8c506d616ee9172
Joshua Wright <jwright@hasborg.com>
2018-10-17 16:26:16 -0400

    Add commands to install VLC

--- update-labs/update-labs-d01.sh
+++ update-labs/update-labs-d01.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
+# Install VLC to play the Pavlok capture video
+apt-get update && apt-get install -y vlc
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
