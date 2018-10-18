23334b617a2a7ec99050ca6bf4e554f3668086fe
Joshua Wright <jwright@hasborg.com>
2018-10-18 08:32:47 -0400

    Undo vlc install

--- update-labs/update-labs-d01.sh
+++ update-labs/update-labs-d01.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
-# Install VLC to play the Pavlok capture video
-apt-get update && apt-get install -y vlc
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
