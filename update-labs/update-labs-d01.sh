3305442d5ac2c4b36b37a81e62fc2b946a82a133
Joshua Wright <jwright@hasborg.com>
2019-06-17 21:24:50 -0400

    Add git pull for btfind to add scrolling fix

--- update-labs/update-labs-d01.sh
+++ update-labs/update-labs-d01.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
+pushd . >/dev/null
+cd /root/sec617/bluetooth/btfind/
+git pull >/dev/null
+popd . >/dev/null
 
 echo "Update complete!"
 
