dabc22d2c6fdcbf7e22b3c49b0fe7183837490eb
Joshua Wright <jwright@hasborg.com>
2019-06-25 10:57:08 -0400

    Fix incorrect path in update-labs SEC504 download

--- update-labs/sec504/update-labs-e01.sh
+++ update-labs/sec504/update-labs-e01.sh
@@ -4,8 +4,8 @@
 sudo rm /opt/Responder/Responder.db
 pushd . >/dev/null
 cd /tmp
-wget https://joswr1ght.github.io/update-labs/stop.sh
-wget https://joswr1ght.github.io/update-labs/xssautorun.sh
+wget https://joswr1ght.github.io/update-labs/sec504/stop.sh
+wget https://joswr1ght.github.io/update-labs/sec504/xssautorun.sh
 mv stop.sh /home/sec504/CourseFiles/weblab/stop.sh
 mv xssautorun.sh /home/sec504/.quick/xssautorun.sh
 popd >/dev/null
