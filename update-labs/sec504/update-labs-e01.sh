a0571ac1cacd5319cd5136a0198ff2a9004e14be
Joshua Wright <jwright@hasborg.com>
2019-06-25 10:54:08 -0400

    Add SEC504 update commands to deploy XSS autorun script

--- update-labs/sec504/update-labs-e01.sh
+++ update-labs/sec504/update-labs-e01.sh
@@ -2,6 +2,14 @@
 { # this ensures the entire script is downloaded #
 
 sudo rm /opt/Responder/Responder.db
+pushd . >/dev/null
+cd /tmp
+wget https://joswr1ght.github.io/update-labs/stop.sh
+wget https://joswr1ght.github.io/update-labs/xssautorun.sh
+mv stop.sh /home/sec504/CourseFiles/weblab/stop.sh
+mv xssautorun.sh /home/sec504/.quick/xssautorun.sh
+popd >/dev/null
+
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
