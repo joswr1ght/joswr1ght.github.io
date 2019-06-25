7c07e5fd668ac98b988316264fc3b59d3820ae38
Joshua Wright <jwright@hasborg.com>
2019-06-25 11:21:25 -0400

    Fix script, make XSS lab scripts executable

--- update-labs/sec504/update-labs-e01.sh
+++ update-labs/sec504/update-labs-e01.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
+# Remove default Responder.db file
 sudo rm /opt/Responder/Responder.db
+
+# Update XSS lab autorun script
 pushd . >/dev/null
 cd /tmp
 wget https://joswr1ght.github.io/update-labs/sec504/stop.sh
 wget https://joswr1ght.github.io/update-labs/sec504/xssautorun.sh
 mv stop.sh /home/sec504/CourseFiles/weblab/stop.sh
 mv xssautorun.sh /home/sec504/.quick/xssautorun.sh
+chmod 755 /home/sec504/CourseFiles/weblab/stop.sh
+chmod 755 /home/sec504/.quick/xssautorun.sh
 popd >/dev/null
 
 echo "Update complete!"
