ccfbbb35d55b8e7b2351a3efe3bb1f8ccc70b033
Joshua Wright <jwright@hasborg.com>
2019-07-15 13:06:25 -0400

    Push update script for Slingshot; include SMB target exercise

--- update-labs/sec504/update-labs-e01.sh
+++ update-labs/sec504/update-labs-e01.sh
@@ -15,6 +15,18 @@ chmod 755 /home/sec504/CourseFiles/weblab/stop.sh
 chmod 755 /home/sec504/.quick/xssautorun.sh
 popd >/dev/null
 
+# Update smbtgt lab
+pushd . >/dev/null
+cd /home/sec504/CourseFiles/
+wget https://joswr1ght.github.io/update-labs/sec504/smbtgt.tgz >/dev/null 2>&1
+wget https://joswr1ght.github.io/update-labs/sec504/gosmbtgt >/dev/null 2>&1
+sudo mv gosmbtgt /usr/local/bin/gosmbtgt
+sudo chmod 755 /usr/local/bin/gosmbtgt
+tar xfz smbtgt.tgz
+cd smbtgt
+docker build -t smbtgt . >/dev/null
+popd >/dev/null
+
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
