67beab6c97f855398ffce413489c718168702d15
Joshua Wright <jwright@hasborg.com>
2019-07-18 17:27:20 -0400

    Don't download git-lfs.exe if it already exists

--- update-labs/sec504/update-labs-e01.ps1
+++ update-labs/sec504/update-labs-e01.ps1
@@ -1,11 +1,13 @@
 # Update the Windows 10 VM as needed to address lab problems.
 
-# git-lfs install for lab video walkthrough access
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-Invoke-WebRequest https://joswr1ght.github.io/update-labs/sec504/git-lfs.exe -OutFile c:\tools\git-lfs.exe
-git lfs install
+if (-Not (Test-Path -Path C:\Tools\git-lfs.exe)) {
+    # git-lfs install for lab video walkthrough access
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+    Invoke-WebRequest https://joswr1ght.github.io/update-labs/sec504/git-lfs.exe -OutFile c:\tools\git-lfs.exe
+    git lfs install
+}
 
 # Make sure the Windows Firewall stays disabled
-powershell Start-Process cmd.exe -ArgumentList "/c","netsh","advfirewall","set","allprofiles","state","off" -Verb runAs
+Start-Process cmd.exe -ArgumentList "/c","netsh","advfirewall","set","allprofiles","state","off" -Verb runAs
 
 Write-Host "Update complete!"
