dd7bb985022b07a2bf2c3113f4d84b5cf8d53f3f
Joshua Wright <jwright@hasborg.com>
2019-07-18 09:11:12 -0400

    Update Windows ps1 update-labs to d/l and run git-lfs

--- update-labs/sec504/update-labs-e01.ps1
+++ update-labs/sec504/update-labs-e01.ps1
@@ -1,2 +1,8 @@
 # Update the Windows 10 VM as needed to address lab problems.
+
+# git-lfs install for lab video walkthrough access
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+Invoke-WebRequest https://joswr1ght.github.io/update-labs/sec504/git-lfs.exe -OutFile c:\tools\git-lfs.exe
+git lfs install
+
 Write-Host "Update complete!"
