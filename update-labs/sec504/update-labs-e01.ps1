c927681aa469324424d6505c42ebf185ccdc489f
Joshua Wright <jwright@hasborg.com>
2019-07-20 06:36:04 -0400

    Update Windows update-labs-e01.ps1 script to retrieve modified update-wiki.ps1 script with network conn test change

--- update-labs/sec504/update-labs-e01.ps1
+++ update-labs/sec504/update-labs-e01.ps1
@@ -1,5 +1,9 @@
 # Update the Windows 10 VM as needed to address lab problems.
 
+# Replace the update-labs.ps1 script with an updated version
+Invoke-WebRequest https://joswr1ght.github.io/update-labs/sec504/update-wiki.ps1 -OutFile c:\tools\update-wiki.ps1
+
+# Download and configure git lfs for lab videos
 if (-Not (Test-Path -Path C:\Tools\git-lfs.exe)) {
     # git-lfs install for lab video walkthrough access
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
