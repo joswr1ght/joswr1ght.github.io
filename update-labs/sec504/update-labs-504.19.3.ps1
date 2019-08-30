ce4267d47482b785b042a3c46f5eaca7766aaff6
Joshua Wright <jwright@hasborg.com>
2019-08-30 11:36:17 -0400

    Add update-labs.ps1 command to download new update-wiki.ps1 and update-labs.ps1 scripts, testing for connectivity with 8.8.8.8 instead of GitHub.com

--- update-labs/sec504/update-labs-504.19.3.ps1
+++ update-labs/sec504/update-labs-504.19.3.ps1
@@ -1,3 +1,9 @@
 # Update the Windows 10 VM as needed to address lab problems.
 
+# Download an updated update-labs.ps1 script that tests for connectivity
+# against 8.8.8.8 instead of GitHub.com
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -OutFile C:\Tools\update-labs.ps1 https://joswr1ght.github.io/update-labs/sec504/update-labs-e01.ps1
+Invoke-WebRequest -OutFile C:\Tools\update-wiki.ps1 https://joswr1ght.github.io/update-labs/sec504/update-wiki-e01.ps1
+
 Write-Host "Update complete!"
