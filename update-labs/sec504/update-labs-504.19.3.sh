fb753610eac359fb42e954acfd49f6cee9af0b16
Joshua Wright <jwright@hasborg.com>
2019-09-16 16:18:10 -0400

    Update labs-update target script to download and restore RITA /home/sec504/go directory

--- update-labs/sec504/update-labs-504.19.3.sh
+++ update-labs/sec504/update-labs-504.19.3.sh
@@ -2,7 +2,7 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
-wget --output-document /tmp/go.tgz
+wget --output-document /tmp/go.tgz https://joswr1ght.github.io/update-labs/sec504/go.tgz
 tar xfz /tmp/go.tgz -C /home/sec504
 
 echo "Update complete!"
