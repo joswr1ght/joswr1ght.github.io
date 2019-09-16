4806cf39b8e16a4f499326238f59bd811138b03e
Joshua Wright <jwright@hasborg.com>
2019-09-16 16:15:17 -0400

    Add go tarball to fix RITA problem

--- update-labs/sec504/update-labs-504.19.3.sh
+++ update-labs/sec504/update-labs-504.19.3.sh
@@ -2,6 +2,8 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
+wget --output-document /tmp/go.tgz
+tar xfz /tmp/go.tgz -C /home/sec504
 
 echo "Update complete!"
 
