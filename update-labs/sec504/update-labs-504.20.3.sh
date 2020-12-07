5d7c4a7f7b75ddc1ba1ded7be4c3f5330f02ada7
Joshua Wright <jwright@hasborg.com>
2020-12-07 06:29:33 -0500

    Better fix for 504.20.3 Slingshot problematic Docker labs

--- update-labs/sec504/update-labs-504.20.3.sh
+++ update-labs/sec504/update-labs-504.20.3.sh
@@ -8,19 +8,15 @@ cd $HOME/wiki
 git pull >/dev/null 2>/dev/null
 git reset --hard origin/MinorUpdate2020.2
 
-
-# Fix problem with SSH pivot lab and Drupal target unable to connect back
-set -euo pipefail
-
 stoppivottgt >/dev/null 2>&1 || true
 
 pushd . >/dev/null
+
+echo Updating lab-specific scripts and files
 cd $HOME/labs
-git checkout bin/update-labs
-git checkout weblab/php.log
+git checkout bin/update-labs >/dev/null
+git checkout weblab/php.log >/dev/null
 git pull >/dev/null
-echo Network temporarily offline while rebuilding Docker target
-sudo ifconfig eth0 down
 
 echo Rebuilding Docker target for pivot lab
 cd $HOME/labs/pivotlab/drupal
@@ -34,11 +30,7 @@ echo Rebuilding Docker target for Bootcamp: Olympic Event
 cd $HOME/labs/bootcamp/bc-olympicevent
 ./build.sh >/dev/null 2>/dev/null
 
-sudo ifconfig eth0 up
-echo Done.
 popd >/dev/null
-connect-net
-
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
