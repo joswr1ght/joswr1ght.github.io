0208796552bcb6b3d3858e88fad8cf51af242476
Joshua Wright <jwright@hasborg.com>
2021-03-19 13:47:00 -0400

    Add symlink to connect-ctf in update script

--- update-labs/sec504/update-labs-504.20.4.sh
+++ update-labs/sec504/update-labs-504.20.4.sh
@@ -2,7 +2,7 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
-
+sudo ln -s /home/sec504/labs/bin/connect-ctf /usr/local/bin/connect-ctf
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
