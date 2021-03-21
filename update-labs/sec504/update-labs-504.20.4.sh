da3102d396300ea988663870db8e8a3b1959cc97
Joshua Wright <jwright@hasborg.com>
2021-03-21 08:12:32 -0400

    Distribute /etc/hosts changes to 504.20.4 ISO media on Slingshot

--- update-labs/sec504/update-labs-504.20.4.sh
+++ update-labs/sec504/update-labs-504.20.4.sh
@@ -3,6 +3,7 @@
 
 echo "Running lab update script."
 sudo ln -s /home/sec504/labs/bin/connect-ctf /usr/local/bin/connect-ctf
+wget -qO- https://www.willhackforsushi.com/hosts | sudo tee /etc/hosts >/dev/null
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
