86ff3e3dc46f81b5aa4628395d64df5884b3ebbd
Joshua Wright <jwright@hasborg.com>
2021-09-30 07:28:26 -0400

    Tweak update-labs 504.21.2 target

--- update-labs/sec504/update-labs-504.21.2.sh
+++ update-labs/sec504/update-labs-504.21.2.sh
@@ -4,13 +4,11 @@
 echo "Running lab update script."
 
 echo "Updating JSON Bootcamp."
-pushd
-connect-net
-cd /home/sec504/labs
-git pull
-cd bootcamp/bc-json
-./build.sh
-connect-lab
+pushd .
+(connect-net && cd /home/sec504/labs && git pull && cd bootcamp/bc-json && ./build.sh && connect-lab) >/tmp/update-labs.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Update process experienced an error. See /tmp/update-labs.log and contact the instructor for assistance."
+fi
 popd
 echo "Update complete!"
 
