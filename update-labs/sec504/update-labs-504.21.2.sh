c431d2d83c8ec679563781bc706ac9d05cc49884
Joshua Wright <jwright@hasborg.com>
2021-09-30 07:13:55 -0400

    Add 504.21.2 steps to fix incorrect bc-json hints

--- update-labs/sec504/update-labs-504.21.2.sh
+++ update-labs/sec504/update-labs-504.21.2.sh
@@ -2,6 +2,16 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
+
+echo "Updating JSON Bootcamp."
+pushd
+connect-net
+cd /home/sec504/labs
+git pull
+cd bootcamp/bc-json
+./build.sh
+connect-lab
+popd
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
