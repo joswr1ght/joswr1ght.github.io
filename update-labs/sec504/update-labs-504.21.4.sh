faae8301bdcdc28bab5932da472c4ad33ebf86ce
Joshua Wright <jwright@hasborg.com>
2021-12-09 10:58:06 -0500

    Add rebuild for Docker weblab, bc-files, bc-json to 504.21.4 script

--- update-labs/sec504/update-labs-504.21.4.sh
+++ update-labs/sec504/update-labs-504.21.4.sh
@@ -2,6 +2,11 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
+pushd .
+cd /home/sec504/labs/weblab && ./build.sh >>/tmp/update-labs.log
+cd /home/sec504/labs/bootcamp/bc-files && ./build.sh >>/tmp/update-labs.log
+cd /home/sec504/labs/bootcamp/bc-json && ./build.sh >>/tmp/update-labs.log
+popd
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
