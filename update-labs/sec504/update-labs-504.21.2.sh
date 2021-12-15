555522cea4431134716fbf7567f0dbc095b3d306
Joshua Wright <jwright@hasborg.com>
2021-12-15 10:19:08 -0500

    Update 504.21.2 update script to rebuild S3 container

--- update-labs/sec504/update-labs-504.21.2.sh
+++ update-labs/sec504/update-labs-504.21.2.sh
@@ -5,7 +5,7 @@ echo "Running lab update script."
 
 echo "Updating containers."
 pushd . >/dev/null
-(cd /home/sec504/labs && git pull && cd bootcamp/bc-json && ./build.sh && cd ../bc-networking && ./build.sh && cd ../bc-olympicevent && ./build.sh && cd ../../weblab && ./build.sh) >/tmp/update-labs.log 2>&1
+(cd /home/sec504/labs && git pull && cd bootcamp/bc-json && ./build.sh && cd ../bc-networking && ./build.sh && cd ../bc-olympicevent && ./build.sh && cd ../../weblab && ./build.sh && cd ../s3/s3 && ./build.sh) >/tmp/update-labs.log 2>&1
 if [ $? -ne 0 ]; then
     echo "Update process experienced an error. See /tmp/update-labs.log and contact the instructor for assistance."
 fi
