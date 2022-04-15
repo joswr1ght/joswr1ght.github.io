0210807b6f07c66053a55f3bb99d3eb39992e197
Joshua Wright <jwright@hasborg.com>
2022-04-15 12:52:48 -0400

    Add sec504-slingshot hosts fix to 504.21.4.sh

--- update-labs/sec504/update-labs-504.21.4.sh
+++ update-labs/sec504/update-labs-504.21.4.sh
@@ -6,6 +6,7 @@ pushd .
 cd /home/sec504/labs/weblab && ./build.sh >>/tmp/update-labs.log
 cd /home/sec504/labs/bootcamp/bc-files && ./build.sh >>/tmp/update-labs.log
 cd /home/sec504/labs/bootcamp/bc-json && ./build.sh >>/tmp/update-labs.log
+sudo sed -i '1 s/127.0.0.1.*/127.0.0.1 localhost slingshot sec504-slingshot/' /etc/hosts
 popd
 echo "Update complete!"
 
