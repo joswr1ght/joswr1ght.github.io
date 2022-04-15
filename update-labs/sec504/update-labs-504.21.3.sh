afe1124090c967795825e93e4c24f0ef0f9ae4f8
Joshua Wright <jwright@hasborg.com>
2022-04-15 12:45:53 -0400

    Update 504.21.3.sh to add sec504-slingshot to /etc/hosts file

--- update-labs/sec504/update-labs-504.21.3.sh
+++ update-labs/sec504/update-labs-504.21.3.sh
@@ -2,6 +2,7 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
+sudo sed -i '1 s/127.0.0.1.*/127.0.0.1 localhost slingshot sec504-slingshot/' /etc/hosts
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
