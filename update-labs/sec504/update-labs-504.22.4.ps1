92d4a14c0f862427bbe46e71c6cbbe586b1385ea
Joshua Wright <jwright@hasborg.com>
2022-08-07 06:03:34 -0400

    Add download of updated PSO TOML file to update-labs 504.22.4

--- update-labs/sec504/update-labs-504.22.4.ps1
+++ update-labs/sec504/update-labs-504.22.4.ps1
@@ -1,3 +1,3 @@
 # Update the Windows 10 VM as needed to address lab problems.
-
+iwr https://joswr1ght.github.io/update-labs/sec504/psolympics-gettingstarted.toml -OutFile C:\tools\psolympics\psolympics-gettingstarted.toml
 Write-Host "Update complete!"
