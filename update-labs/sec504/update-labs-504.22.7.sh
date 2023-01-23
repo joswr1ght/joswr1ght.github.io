960437faa50c5685c205a74df63def4d7f92d5a1
Joshua Wright <jwright@hasborg.com>
2023-01-23 06:43:57 -0500

    Add empty PS1 scripts for update-labs for 504.22.6+7, add Slingshot script to remove foo.ovpn for 504.22.7

---
+++ update-labs/sec504/update-labs-504.22.7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{ # this ensures the entire script is downloaded #
+
+echo "Running lab update script."
+rm ~/Downloads/foo.ovpn 2>/dev/null
+echo "Update complete!"
+
+} # this ensures the entire script is downloaded #
