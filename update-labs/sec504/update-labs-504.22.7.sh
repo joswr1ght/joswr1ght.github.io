877c2a23f174055ac6320df5a0ff0aba66fe65e1
Joshua Wright <jwright@hasborg.com>
2023-02-01 10:34:28 -0500

    Undo git-tracked connect-ctf changes in labs/bin

--- update-labs/sec504/update-labs-504.22.7.sh
+++ update-labs/sec504/update-labs-504.22.7.sh
@@ -3,6 +3,7 @@
 
 echo "Running lab update script."
 rm ~/Downloads/foo.ovpn 2>/dev/null
+cd ~/labs/bin && git checkout connect-ctf
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
