e58259e0b7298ed81d96912121543fc664b6a0f7
Joshua Wright <jwright@hasborg.com>
2023-02-04 15:46:52 -0500

    Fix update-labs to not error on update

--- update-labs/sec504/update-labs-504.22.7.sh
+++ update-labs/sec504/update-labs-504.22.7.sh
@@ -2,8 +2,11 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
+pushd . >/dev/null
 rm ~/Downloads/foo.ovpn 2>/dev/null
-cd ~/labs/bin && git checkout connect-ctf
+cd ~/labs && git stash 2>/dev/nul
+git pull
+popd >/dev/null
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
