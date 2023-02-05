8f509e37307a6de05d8b7a48d6e9eab2e1aa79cc
Joshua Wright <jwright@hasborg.com>
2023-02-04 19:48:11 -0500

    Adjust 504.22.5.sh to fix typo

--- update-labs/sec504/update-labs-504.22.5.sh
+++ update-labs/sec504/update-labs-504.22.5.sh
@@ -4,7 +4,7 @@
 echo "Running lab update script."
 pushd . >/dev/null
 rm ~/Downloads/foo.ovpn 2>/dev/null
-cd ~/labs && git stash 2>/dev/nul
+cd ~/labs && git checkout bin/connect-ctf >/dev/null 2>/dev/null
 git pull
 popd >/dev/null
 echo "Update complete!"
