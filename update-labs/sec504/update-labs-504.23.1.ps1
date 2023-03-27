f65991d5bc3fc28124b022c7a5e1f429f7cd2295
Joshua Wright <jwright@hasborg.com>
2023-03-27 06:29:28 -0400

    Add GitHub RSA key fix to 504.22.7 and 504.23.1 sh/ps1 scripts

--- update-labs/sec504/update-labs-504.23.1.ps1
+++ update-labs/sec504/update-labs-504.23.1.ps1
@@ -1,4 +1,3 @@
 # Update the Windows 10 VM as needed to address lab problems.
-
 ssh-keygen -R github.com
 Write-Host "Update complete!"
--- update-labs/sec504/update-labs-504.23.1.sh
+++ update-labs/sec504/update-labs-504.23.1.sh
@@ -2,12 +2,7 @@
 { # this ensures the entire script is downloaded #
 
 echo "Running lab update script."
-pushd . >/dev/null
-rm ~/Downloads/foo.ovpn 2>/dev/null
-cd ~/labs && git stash 2>/dev/nul
-git pull
 ssh-keygen -R github.com
-popd >/dev/null
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
