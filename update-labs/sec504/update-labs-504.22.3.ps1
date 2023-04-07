107b3312e523a0c5e936eca2cd37871fa3b35023
Joshua Wright <jwright@hasborg.com>
2023-04-07 16:07:56 -0400

    Update several update-labs scripts for GitHub key revocation issue

--- update-labs/sec504/update-labs-504.22.3.ps1
+++ update-labs/sec504/update-labs-504.22.3.ps1
@@ -1,3 +1,4 @@
 # Update the Windows 10 VM as needed to address lab problems.
 iwr https://joswr1ght.github.io/update-labs/sec504/psolympics-gettingstarted.toml -OutFile C:\tools\psolympics\psolympics-gettingstarted.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/known_hosts.win10.github -OutFile c:\users\sec504\.ssh\known_hosts
 Write-Host "Update complete!"
--- update-labs/sec504/update-labs-504.22.7.ps1
+++ update-labs/sec504/update-labs-504.22.7.ps1
@@ -1,4 +1,4 @@
 # Update the Windows 10 VM as needed to address lab problems.
 
-ssh-keygen -R github.com
+iwr https://joswr1ght.github.io/update-labs/sec504/known_hosts.win10.github -OutFile c:\users\sec504\.ssh\known_hosts
 Write-Host "Update complete!"
