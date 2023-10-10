11afb5d43ee7b8077eca4aa256d2df0fb08893fe
Joshua Wright <jwright@hasborg.com>
2023-10-09 20:00:04 -0500

    Add steps to recreate dir and file for PS1 profile for PSO

--- update-labs/sec504/update-labs-504.23.1.ps1
+++ update-labs/sec504/update-labs-504.23.1.ps1
@@ -1,3 +1,5 @@
 # Update the Windows 10 VM as needed to address lab problems.
 iwr https://joswr1ght.github.io/update-labs/sec504/known_hosts.win10.github -OutFile c:\users\sec504\.ssh\known_hosts
+mkdir C:\users\sec504\Documents\WindowsPowerShell -ea SilentlyContinue
+Out-Null | Out-File C:\users\sec504\Documents\WindowsPowerShell\microsoft.powershell_profile.ps1 -ea SilentlyContinue
 Write-Host "Update complete!"
