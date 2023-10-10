d3884da1b565d2b48cbe9d7e6b2510050c0e4661
Joshua Wright <jwright@hasborg.com>
2023-10-09 20:07:49 -0500

    Replace psolympics.toml

--- update-labs/sec504/update-labs-504.23.1.ps1
+++ update-labs/sec504/update-labs-504.23.1.ps1
@@ -1,5 +1,7 @@
 # Update the Windows 10 VM as needed to address lab problems.
 iwr https://joswr1ght.github.io/update-labs/sec504/known_hosts.win10.github -OutFile c:\users\sec504\.ssh\known_hosts
+remove-item c:\tools\psolympics\psolympics.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/psolympics.toml -OutFile c:\tools\psolympics\psolympics.toml
 mkdir C:\users\sec504\Documents\WindowsPowerShell -ea SilentlyContinue
 Out-Null | Out-File C:\users\sec504\Documents\WindowsPowerShell\microsoft.powershell_profile.ps1 -ea SilentlyContinue
 Write-Host "Update complete!"
