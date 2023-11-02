e02b306232005e9ee49aa0c88ab2761eefb1a445
Joshua Wright <jwright@hasborg.com>
2023-11-02 11:57:17 -0400

    Add 504.23.1 PS1 update for new PSO utilities

--- update-labs/sec504/update-labs-504.23.1.ps1
+++ update-labs/sec504/update-labs-504.23.1.ps1
@@ -4,4 +4,8 @@ remove-item c:\tools\psolympics\psolympics.toml
 iwr https://joswr1ght.github.io/update-labs/sec504/psolympics.toml -OutFile c:\tools\psolympics\psolympics.toml
 mkdir C:\users\sec504\Documents\WindowsPowerShell -ea SilentlyContinue
 Out-Null | Out-File C:\users\sec504\Documents\WindowsPowerShell\microsoft.powershell_profile.ps1 -ea SilentlyContinue
+Rename-Item C:\Tools\psolympics\tpane.exe C:\Tools\psolympics\tpane.exe.old
+Rename-Item C:\Tools\psolympics\launcher.exe C:\Tools\psolympics\launcher.exe.old
+iwr https://joswr1ght.github.io/update-labs/sec504/tpane.exe -OutFile C:\Tools\psolympics\tpane.exe
+iwr https://joswr1ght.github.io/update-labs/sec504/launcher.exe -OutFile C:\Tools\psolympics\launcher.exe
 Write-Host "Update complete!"
