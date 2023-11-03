a1958860875b69e1bd5bc033a79932d7149b1e1f
Joshua Wright <jwright@hasborg.com>
2023-11-03 12:52:12 -0400

    Restore execute on lightninglabs.py

--- update-labs/sec504/update-labs-504.23.3.sh
+++ update-labs/sec504/update-labs-504.23.3.sh
@@ -8,6 +8,7 @@ git pull >/dev/null
 
 # Move WeTTY to TCP/3001
 curl -s -L https://joswr1ght.github.io/update-labs/sec504/lightninglabs-20231103.tgz | tar xz -C /opt
+sudo chmod 755 /opt/lightninglabs/src/lightninglabs.py
 llrestart
 
 popd 2>&1
--- update-labs/sec504/update-labs-504.23.5.sh
+++ update-labs/sec504/update-labs-504.23.5.sh
@@ -6,6 +6,7 @@ pushd . >/dev/null 2>&1
 
 # Move WeTTY to TCP/3001
 curl -s -L https://joswr1ght.github.io/update-labs/sec504/lightninglabs-20231103.tgz | tar xz -C /opt
+sudo chmod 755 /opt/lightninglabs/src/lightninglabs.py
 llrestart
 
 popd 2>&1
