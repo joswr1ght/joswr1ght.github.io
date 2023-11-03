9d0b1688eca35525677bc1f28855114072a79513
Joshua Wright <jwright@hasborg.com>
2023-11-02 22:45:56 -0400

    Update 504.23.5 Windows VM to use updated PSO files and executables

--- update-labs/sec504/update-labs-504.23.5.ps1
+++ update-labs/sec504/update-labs-504.23.5.ps1
@@ -1,2 +1,13 @@
 # Update the Windows 10 VM as needed to address lab problems.
+mkdir C:\users\sec504\Documents\WindowsPowerShell -ea SilentlyContinue
+Out-Null | Out-File C:\users\sec504\Documents\WindowsPowerShell\microsoft.powershell_profile.ps1 -ea SilentlyContinue
+
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics.toml -OutFile c:\tools\psolympics\psolympics.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/tpane.exe -OutFile C:\Tools\psolympics\tpane.exe
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/launcher.exe -OutFile C:\Tools\psolympics\launcher.exe
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-drives.toml -OutFile C:\Tools\psolympics\psolympics-drives.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-gettingstarted.toml -OutFile C:\Tools\psolympics\psolympics-gettingstarted.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-pipelines.toml -OutFile C:\Tools\psolympics\psolympics-pipelines.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-variables.toml -OutFile C:\Tools\psolympics\psolympics-variables.toml
+
 Write-Host "Update complete!"
