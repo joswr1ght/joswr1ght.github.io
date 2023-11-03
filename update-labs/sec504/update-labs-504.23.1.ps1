d6f37a067fb56b2aa0c15fe4e56ede6946bd9e16
Joshua Wright <jwright@hasborg.com>
2023-11-02 22:44:35 -0400

    Replace PSO TOML files, updated for / pathing in ps matches

--- update-labs/sec504/update-labs-504.23.1.ps1
+++ update-labs/sec504/update-labs-504.23.1.ps1
@@ -1,11 +1,15 @@
 # Update the Windows 10 VM as needed to address lab problems.
 iwr https://joswr1ght.github.io/update-labs/sec504/known_hosts.win10.github -OutFile c:\users\sec504\.ssh\known_hosts
-remove-item c:\tools\psolympics\psolympics.toml
-iwr https://joswr1ght.github.io/update-labs/sec504/psolympics.toml -OutFile c:\tools\psolympics\psolympics.toml
+
 mkdir C:\users\sec504\Documents\WindowsPowerShell -ea SilentlyContinue
 Out-Null | Out-File C:\users\sec504\Documents\WindowsPowerShell\microsoft.powershell_profile.ps1 -ea SilentlyContinue
-Rename-Item C:\Tools\psolympics\tpane.exe C:\Tools\psolympics\tpane.exe.old
-Rename-Item C:\Tools\psolympics\launcher.exe C:\Tools\psolympics\launcher.exe.old
-iwr https://joswr1ght.github.io/update-labs/sec504/tpane.exe -OutFile C:\Tools\psolympics\tpane.exe
-iwr https://joswr1ght.github.io/update-labs/sec504/launcher.exe -OutFile C:\Tools\psolympics\launcher.exe
+
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics.toml -OutFile c:\tools\psolympics\psolympics.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/tpane.exe -OutFile C:\Tools\psolympics\tpane.exe
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/launcher.exe -OutFile C:\Tools\psolympics\launcher.exe
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-drives.toml -OutFile C:\Tools\psolympics\psolympics-drives.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-gettingstarted.toml -OutFile C:\Tools\psolympics\psolympics-gettingstarted.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-pipelines.toml -OutFile C:\Tools\psolympics\psolympics-pipelines.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-variables.toml -OutFile C:\Tools\psolympics\psolympics-variables.toml
+
 Write-Host "Update complete!"
