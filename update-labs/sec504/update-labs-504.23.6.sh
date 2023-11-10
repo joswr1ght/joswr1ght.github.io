a39d65e38dbd183ae3246e270d8c3442087019d4
Joshua Wright <jwright@hasborg.com>
2023-11-10 14:38:34 -0500

    Add empty 504.23.6 update scripts

---
+++ update-labs/sec504/update-labs-504.23.6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+{ # this ensures the entire script is downloaded #
+
+echo "Running lab update script."
+pushd .
+popd
+echo "Update complete!"
+
+} # this ensures the entire script is downloaded #
