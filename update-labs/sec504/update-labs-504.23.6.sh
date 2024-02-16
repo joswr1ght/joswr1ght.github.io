d0e1d6b8f19951fa064adf2fced3d2a3421fa0e8
Joshua Wright <jwright@hasborg.com>
2024-02-16 12:56:36 -0500

    Modify update-labs to add symlinks to display4k/displayauto

--- update-labs/sec504/update-labs-504.23.6.sh
+++ update-labs/sec504/update-labs-504.23.6.sh
@@ -3,6 +3,8 @@
 
 echo "Running lab update script."
 pushd .
+sudo ln -s /home/sec504/bin/display4k /usr/local/bin/display4k
+sudo ln -s /home/sec504/bin/displayauto /usr/local/bin/displayauto
 popd
 echo "Update complete!"
 
