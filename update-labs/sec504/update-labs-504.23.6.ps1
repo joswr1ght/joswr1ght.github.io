15e4a703fe3d2414d179411a10dc571231f1395f
Joshua Wright <jwright@hasborg.com>
2024-02-20 09:06:22 -0500

    Update PS1 504.23.6 update-labs to pull fixes for PSO event files

--- update-labs/sec504/update-labs-504.23.6.ps1
+++ update-labs/sec504/update-labs-504.23.6.ps1
@@ -1,3 +1,6 @@
 # Update the Windows 10 VM as needed to address lab problems.
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-drives.toml -OutFile C:\Tools\psolympics\psolympics-drives.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-pipelines.toml -OutFile C:\Tools\psolympics\psolympics-pipelines.toml
+iwr https://joswr1ght.github.io/update-labs/sec504/pso/psolympics-variables.toml -OutFile C:\Tools\psolympics\psolympics-variables.toml
 
 Write-Host "Update complete!"
