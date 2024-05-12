dd1ac8b87946b4d8b6f289d267ed71b3e702b402
Joshua Wright <jwright@hasborg.com>
2024-05-12 06:28:59 -0400

    Remove stale SSH key from 504.24.1

--- update-labs/sec504/update-labs-504.24.1.sh
+++ update-labs/sec504/update-labs-504.24.1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
+# Remove stale SSH key from
+ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "10.142.145.110" >/dev/null 2>&1
+
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
