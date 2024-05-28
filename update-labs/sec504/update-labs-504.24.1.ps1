70aaed6f368805261773a296862d81939b6c3621
Joshua Wright <jwright@hasborg.com>
2024-05-28 06:04:16 -0400

    Add git config set-url origin to remove SSH over port 443 on Windows VM

--- update-labs/sec504/update-labs-504.24.1.ps1
+++ update-labs/sec504/update-labs-504.24.1.ps1
@@ -1,5 +1,6 @@
 # Update the Windows 10 VM as needed to address lab problems.
 
 # Replace update-wiki.ps1 with an edit to SSH over port 22 instead of 443.
+Push-Location . ; Set-Location C:\Wiki ; git remote set-url origin ssh://git@ssh.github.com/joswr1ght/SANS-504-Student-Wiki ; Pop-Location
 iwr https://joswr1ght.github.io/update-labs/sec504/dist/update-wiki.ps1 -OutFile C:\Tools\update-wiki.ps1
 Write-Host "Update complete!"
