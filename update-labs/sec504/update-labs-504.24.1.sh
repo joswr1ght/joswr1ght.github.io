ae3a9de3c3dfe460d4c64e431f7327108d6e598d
Joshua Wright <jwright@hasborg.com>
2024-08-17 05:55:57 -0400

    Fix update-labs to remove SSH key for ops by hostnam and IP

--- update-labs/sec504/update-labs-504.24.1.sh
+++ update-labs/sec504/update-labs-504.24.1.sh
@@ -3,6 +3,7 @@
 
 # Remove stale SSH key from
 ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "10.142.145.110" >/dev/null 2>&1
+ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "ops.issplaylist.com" >/dev/null 2>&1
 
 echo "Update complete!"
 
