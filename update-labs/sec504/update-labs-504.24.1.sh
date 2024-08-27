ebdb2d8b999e2ab4e7782edd8dbb3fbc21cab525
Joshua Wright <jwright@hasborg.com>
2024-08-27 17:03:14 -0400

    Add Firefox cleanup steps

--- update-labs/sec504/update-labs-504.24.1.sh
+++ update-labs/sec504/update-labs-504.24.1.sh
@@ -5,6 +5,12 @@
 ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "10.142.145.110" >/dev/null 2>&1
 ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "ops.issplaylist.com" >/dev/null 2>&1
 
+# Clean Firefox history
+find /home/sec504/.mozilla/firefox -name key4.db -print -exec rm {} \; >/dev/null 2>&1
+rm /home/sec504/.mozilla/firefox/*.default/*.sqlite >/dev/null 2>&1
+rm /home/sec504/.mozilla/firefox/*.default/sessionstore.js >/dev/null 2>&1
+rm -r /home/sec504/.cache/mozilla/firefox/* >/dev/null 2>&1
+
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
--- update-labs/sec504/update-labs-504.24.2.sh
+++ update-labs/sec504/update-labs-504.24.2.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
-# Remove stale SSH key from
-ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "10.142.145.110" >/dev/null 2>&1
-
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
