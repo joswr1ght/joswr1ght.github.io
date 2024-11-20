bc5e0f652eed41bbf3e21a76d0a0704bf18c38e9
Joshua Wright <jwright@hasborg.com>
2024-11-20 15:57:46 -0500

    Suppress error warning in build.sh

--- update-labs/sec504/update-labs-504.24.3.sh
+++ update-labs/sec504/update-labs-504.24.3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
-cd /home/sec504/labs/onlinepasstgt/edirectory && ./build.sh >>/tmp/update-labs.log
+cd /home/sec504/labs/onlinepasstgt/edirectory && ./build.sh >>/tmp/update-labs.log 2>&1
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
