b2ddc829574c7ca48b2a144ad0ef312f501d5bb2
Joshua Wright <jwright@hasborg.com>
2024-11-20 16:01:21 -0500

    Remove old known_hosts entry for 172.30.0.185 host

--- update-labs/sec504/update-labs-504.24.3.sh
+++ update-labs/sec504/update-labs-504.24.3.sh
@@ -2,6 +2,7 @@
 { # this ensures the entire script is downloaded #
 
 cd /home/sec504/labs/onlinepasstgt/edirectory && ./build.sh >>/tmp/update-labs.log 2>&1
+ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "172.30.0.185" >>/tmp/update-labs.log 2>&1
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
