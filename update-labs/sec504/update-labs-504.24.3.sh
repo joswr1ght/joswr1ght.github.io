75bd3dc860a1eb1ed21ce25af0a7c468a1e9d864
Joshua Wright <jwright@hasborg.com>
2024-11-20 15:56:00 -0500

    Add build.sh step for edirectory target 504.24.3

--- update-labs/sec504/update-labs-504.24.3.sh
+++ update-labs/sec504/update-labs-504.24.3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
+cd /home/sec504/labs/onlinepasstgt/edirectory && ./build.sh >>/tmp/update-labs.log
 echo "Update complete!"
 
 } # this ensures the entire script is downloaded #
