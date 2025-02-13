cd79a069a3b9caaf7c58cee64c29d6a352fc6137
Joshua Wright <jwright@hasborg.com>
2025-02-13 07:10:34 -0500

    Correct update-labs 504.24.3 to perform a git pull before rebuilding the edirectory container

--- update-labs/sec504/update-labs-504.24.3.sh
+++ update-labs/sec504/update-labs-504.24.3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 { # this ensures the entire script is downloaded #
 
+cd /home/sec504/labs && git pull >>/tmp/update-labs.log 2>&1
 cd /home/sec504/labs/onlinepasstgt/edirectory && ./build.sh >>/tmp/update-labs.log 2>&1
 ssh-keygen -f "/home/sec504/.ssh/known_hosts" -R "172.30.0.185" >>/tmp/update-labs.log 2>&1
 echo "Update complete!"
